2ecc101c7f9cc133af019074ffb863b7764688b4
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-24 12:45:22 +0200

    task with every() method done

--- js/app.js
+++ js/app.js
@@ -24,6 +24,13 @@ console.log(result);
 
 // Array.prototype.every() // is everyone 19 or older?
 
+const result2 = people.every(person => {
+    let date = (new Date).getFullYear();
+    return (date - person.year) >= 19;
+});
+
+console.log(result2);
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
